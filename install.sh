9638b159f32e6cc5e22fb4c73311b15ae2dac891
Trolbridge <dennis.mitchell.x@gmail.com>
2021-05-30 10:03:10 -0400

    Added Base files

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+sudo apt install neovim -y
+sudo apt install zsh -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
+
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/swap
+mkdir -p ~/.vim/undo
+cd /tmp
+tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+
+#Install Vim-Plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
