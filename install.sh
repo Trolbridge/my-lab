4f2abc07ade38356c0aac3aa49852376bef9bef5
Trolbridge <dennis.mitchell.x@gmail.com>
2021-05-30 12:05:00 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+mkdir -p ~/.vim/backup
+mkdir -p ~/.vim/swap
+mkdir -p ~/.vim/undo
+mkdir -p ~/go_projects/bin
+mkdir -p ~/go_projects/pkg
+mkdir -p ~/go_projects/src
+
+git remote add origin https://github.com/Trolbridge/my-lab.git
+git pull origin master
+git branch -M master
+git config --global user.email "dennis.mitchell.x@gmail.com"
+git config --global user.name "Trolbridge"
+
 #sudo apt update
 #sudo apt upgrade -y
 #sudo apt install neovim -y
@@ -7,12 +21,6 @@
 #sudo apt install npm -y
 #sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/swap
-mkdir -p ~/.vim/undo
-mkdir -p ~/go_projects/bin
-mkdir -p ~/go_projects/pkg
-mkdir -p ~/go_projects/src
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
