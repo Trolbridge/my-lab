247e7240648860cfee471e03458fac5e275e26c2
Trolbridge <dennis.mitchell.x@gmail.com>
2021-05-30 12:02:19 -0400

    update install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #sudo apt update
-sudo apt upgrade -y
-sudo apt install neovim -y
-sudo apt install zsh -y
-sudo apt install nodejs -y
-sudo apt install npm -y
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
+#sudo apt upgrade -y
+#sudo apt install neovim -y
+#sudo apt install zsh -y
+#sudo apt install nodejs -y
+#sudo apt install npm -y
+#sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swap
@@ -14,13 +14,13 @@ mkdir -p ~/go_projects/bin
 mkdir -p ~/go_projects/pkg
 mkdir -p ~/go_projects/src
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 
 #Install Vim-Plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-cd /tmp
-tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+#cd /tmp
+#tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
