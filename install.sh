9d108ad84e489b3b348ee03ba65e972bdf85b966
Trolbridge <dennis.mitchell.x@gmail.com>
2021-05-30 20:36:07 -0400

    Auto-commit: saved install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,10 @@ git config --global user.email "dennis.mitchell.x@gmail.com"
 git config --global user.name "Trolbridge"
 
 sudo apt update
-#sudo apt upgrade -y
+sudo apt upgrade -y
 sudo apt install zsh -y
-#sudo apt install nodejs -y
-#sudo apt install npm -y
+sudo apt install nodejs -y
+sudo apt install npm -y
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 git clone https://github.com/neovim/neovim.git
 cd neovim
@@ -31,5 +31,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-#cd /tmp
-#tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
+cd /tmp
+wget -c https://golang.org/dl/go1.16.4.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.16.4.linux-amd64.tar.gz
