2b0032bc6e8ea0ae7af4a40a2b35a188949aceed
Trolbridge <dennis.mitchell.x@gmail.com>
2021-06-01 09:52:18 -0400

    Added start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git init
+git remote add origin https://github.com/Trolbridge/my-lab.git
+git pull origin master
+git config --global user.email "dennis.mitchell.x@gmail.com"
+git config --global user.name "Trolbridge"
